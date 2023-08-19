484eb1681512a05f65485341855ae338f01187df
VitimReidelas64 <inteligenciasartificiais.ias@gmail.com>
2023-08-18 22:06:30 -0300

    Add .XML

--- index.py
+++ index.py
@@ -1,1 +1,19 @@
+#Imports
+import json
+import os
 import cv2 as cv
+
+class Haar_cascade_ai:
+    def __init__(self, type_id):
+        with open(os.path.join('Json', 'typelist.json')) as list_json:
+            self.type_list = json.load(list_json)
+        self.XML = self.type_list[type_id]
+        self.ai = cv.CascadeClassifier(os.path.join('XMLs', self.XML))
+    def train(self, data):
+        pass
+    
+    def classfier(self, data):
+        pass
+    
+
+ai = Haar_cascade_ai(7)
